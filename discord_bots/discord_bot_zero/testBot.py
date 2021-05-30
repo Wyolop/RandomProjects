381094ed1bdca613c58fc184869f194919d4e060
elehas <elehas@ttu.ee>
2021-05-30 12:38:45 +0300

    Reactions for playing

--- discord_bots/discord_bot_zero/testBot.py
+++ discord_bots/discord_bot_zero/testBot.py
@@ -5,9 +5,12 @@ import os
 client = discord.Client()
 greetings = ["yo", "hi", "hello"]
 b = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+turns = {"0️⃣": "0", "1️⃣": "1", "2️⃣": "2", "3️⃣": "3", "4️⃣": "4", "5️⃣": "5", "6️⃣": "6", "7️⃣": "7", "8️⃣": "8"}
 prev = "O"
 x_player = None
 o_player = None
+ttt_msg = None
+ttt_channel = None
 win = [[0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6]]
 
 
@@ -28,65 +31,93 @@ async def on_message(message):
     if "bad" in msg.replace(" ", ""):
         await message.channel.send(":(")
     if msg.split(" ")[0] == "$play":
-        await tic_tac_toe(message)
+        await tic_tac_toe_new(message)
+
+@client.event
+async def on_reaction_add(reaction, user):
+    if user == client.user:
+        return
+    emoji = reaction.emoji
+    if emoji in turns:
+        await tic_tac_toe(turns.get(emoji), user)
 
 
-async def tic_tac_toe(message):
+async def tic_tac_toe_new(message):
     global b
     global prev
     global x_player
     global o_player
+    global ttt_msg
+    global ttt_channel
+    if ttt_channel is None:
+        ttt_channel = message.channel
+    ttt_msg = await ttt_channel.send(f"{x_player} vs {o_player}\n"
+                                         f"```\n{b[0]}|{b[1]}|{b[2]}"
+                                         f"\n-----\n{b[3]}|{b[4]}|{b[5]}"
+                                         f"\n-----\n{b[6]}|{b[7]}|{b[8]}\n```")
+    for emoji in turns.keys():
+        await ttt_msg.add_reaction(emoji)
+
+
+async def tic_tac_toe(turn, author):
+    global ttt_msg
+    global prev
+    global o_player
+    global x_player
     if x_player is None:
-        x_player = message.author
+        x_player = author
     elif o_player is None:
-        o_player = message.author
-    if message.author != x_player and message.author != o_player:
-        await message.channel.send("You are not playing!")
-        return
-    turn = message.content.split(" ")[-1]
+        o_player = author
     if not turn.isdigit():
-        await message.channel.send("Not an integer.")
+        await ttt_channel.send("Not an integer.")
         return
     if b[int(turn)] == " ":
         if prev == "O":
-            if message.author != x_player:
-                await message.channel.send("Not your turn!")
+            if author != x_player:
+                await ttt_channel.send("Not your turn!")
                 return
             b[int(turn)] = "X"
             prev = "X"
         elif prev == "X":
-            if message.author != o_player:
-                await message.channel.send("Not your turn!")
+            if author != o_player:
+                await ttt_channel.send("Not your turn!")
                 return
             b[int(turn)] = "O"
             prev = "O"
     else:
-        await message.channel.send(f"Space is already occupied, try again.")
-    await message.channel.send(f"```\n{b[0]}|{b[1]}|{b[2]}\n-----\n{b[3]}|{b[4]}|{b[5]}\n-----\n{b[6]}|{b[7]}|{b[8]}\n```")
-    await check_win(message)
+        await ttt_channel.send(f"Space is already occupied, try again.")
+    await ttt_msg.edit(content=f"{x_player} vs {o_player}\n"
+                               f"```\n{b[0]}|{b[1]}|{b[2]}"
+                               f"\n-----\n{b[3]}|{b[4]}|{b[5]}"
+                               f"\n-----\n{b[6]}|{b[7]}|{b[8]}\n```")
+    await check_win()
 
 
-async def check_win(message):
+async def check_win():
     global b
     global prev
     global x_player
     global o_player
+    global ttt_msg
     for w in win:
         if b[w[0]] == b[w[1]] == b[w[2]] != " ":
-            b = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-            prev = "O"
-            await message.channel.send(f"@{x_player if b[w[0]] == 'X' else o_player} has won!")
+            await ttt_channel.send(f"<@{x_player.id if b[w[0]] == 'X' else o_player.id}> has won!")
             with open("standings.csv", "a", newline='') as csvfile:
                 writer = csv.writer(csvfile, delimiter=",")
-                writer.writerow([x_player if b[w[0]] == 'X' else o_player, o_player if b[w[0]] == 'X' else x_player, "winner"])
+                writer.writerow([x_player if b[w[0]] == 'X' else o_player,
+                                 o_player if b[w[0]] == 'X' else x_player,
+                                 "winner"])
+            b = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+            prev = "O"
             x_player = None
             o_player = None
+            ttt_msg = None
     for space in b:
         if space == " ":
             return
-    await message.channel.send("Tie game!")
+    await ttt_channel.send("Tie game!")
     with open("standings.csv", "a", newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=",")
-        writer.writerow([x_player if b[w[0]] == 'X' else o_player, o_player if b[w[0]] == 'X' else x_player, "tie"])
+        writer.writerow([x_player, o_player, "tie"])
 
 client.run(os.getenv("TOKEN"))
